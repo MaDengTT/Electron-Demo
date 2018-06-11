dfc1ea63ecf6a97649f595b51bc7bd783c4cf83a
MaDengTT <1124879085@qq.com>
2018-06-11 17:35:45 +0800

    day_6_11_3

--- src/renderer/pages/friends/mainpage.vue
+++ src/renderer/pages/friends/mainpage.vue
@@ -11,7 +11,18 @@
                           </el-input>
                       </div>
                       <div id="right">
-                          right name
+                          <el-dropdown>
+                              <span class="el-dropdown-link">
+                                  Name <i class="el-icon-arrow-down el-icon--right"></i>
+                              </span>
+                                <el-dropdown-menu slot="dropdown">
+                                    <el-dropdown-item>黄金糕</el-dropdown-item>
+                                    <el-dropdown-item>狮子头</el-dropdown-item>
+                                    <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                                    <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                                    <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                                </el-dropdown-menu>
+                          </el-dropdown>
                       </div>
             </el-header>
             <el-main>
@@ -39,105 +50,103 @@
 </template>
 
 <script>
-import testimg from '../../assets/header_icon_1.jpg';
+import testimg from "../../assets/header_icon_1.jpg";
 export default {
-    data(){
-        return{
-            input:'',
-            testUrl:testimg,
-            items:[
-                {
-                    imgUrl:testimg
-                }
-            ]
+  data() {
+    return {
+      input: "",
+      testUrl: testimg,
+      items: [
+        {
+          imgUrl: testimg
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 <style>
-    body{
-        background-color: #F5F7FA;
-        margin: 0;
-        padding: 0;
-    }
-    /* .el-container{
+body {
+  background-color: #f5f7fa;
+  margin: 0;
+  padding: 0;
+}
+/* .el-container{
         width: 1600px;
     } */
-    #icon{
-        width: 40px;
-        height: 40px;
-        background-color: #ddd;
-    }
-    #title{
-        margin-left: 20px;
-        font-size: 20;
-        color: #5B6378;
-    }
-    #search{
-        margin-left: 60px;
-    }
-    #right{
-        float: right;
-    }
-    #like{
-        float: right;
-    }
-    .el-header{
-        background-color: #fff;
-        line-height: 60px;
-        box-shadow: 0px 1px 1px 0px #e2e0e0;
-    }
-    .el-col{
-        background-color: #ccc
-    }
-    .div-line{
-        float: left;
-    }
-    .item{
-        float: left;
-        margin-left: 24px;
-        margin-top: 24px;
-
-    }
-    .box-card{
-        width: 304px;
-        height: 360px;
-    }
-    #itembody{
-        float: right;
-        width: 100%;
-        text-align: center;
-    }
-    img#avatar {
-    width: 120px;
-    height: 120px;
-    border-radius: 100px;
-    margin-top: 30px;
-    }
-    div#name {
-    font-size: 18px;
-    color: #000;
-    font-family: 微软雅黑;
-    margin-top: 16px;
+#icon {
+  width: 40px;
+  height: 40px;
+  background-color: #ddd;
 }
-
+#title {
+  margin-left: 20px;
+  font-size: 20;
+  color: #5b6378;
+}
+#search {
+  margin-left: 60px;
+}
+#right {
+  float: right;
+}
+#like {
+  float: right;
+}
+.el-header {
+  background-color: #fff;
+  line-height: 60px;
+  box-shadow: 0px 1px 1px 0px #e2e0e0;
+}
+.el-col {
+  background-color: #ccc;
+}
+.div-line {
+  float: left;
+}
+.item {
+  float: left;
+  margin-left: 24px;
+  margin-top: 24px;
+}
+.box-card {
+  width: 304px;
+  height: 360px;
+}
+#itembody {
+  float: right;
+  width: 100%;
+  text-align: center;
+}
+img#avatar {
+  width: 120px;
+  height: 120px;
+  border-radius: 100px;
+  margin-top: 30px;
+}
+div#name {
+  font-size: 18px;
+  color: #000;
+  font-family: 微软雅黑;
+  margin-top: 16px;
+}
 
 div#info {
-    font-size: 14px;
-    color: #98A0A6;
-    margin-top: 16px;
+  font-size: 14px;
+  color: #98a0a6;
+  margin-top: 16px;
 }
 div#bottom {
-    width: 100%;
-    float: right;
-    position: relative;
-    margin-top: 24px;
+  width: 100%;
+  float: right;
+  position: relative;
+  margin-top: 24px;
 }
 .bottomdiv {
-    float: right;
-    width: 50%;
-    color: #98A0A6;
-    font-size: 14px;
+  float: right;
+  width: 50%;
+  color: #98a0a6;
+  font-size: 14px;
 }
 </style>
 
